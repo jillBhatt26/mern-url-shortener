2a411cb2acd4244ebd0d297714828c4e1096888e
jillbhatt26 <bhattjill@gmail.com>
2021-07-03 00:04:29 +0530

    Finished config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,44 +1,5 @@
-// module and types imports
-import express, { Application } from 'express';
-import { connect } from 'mongoose';
-import dotenv from 'dotenv';
+import Database from './config/Database';
 
-// Setup
-const app: Application = express();
-dotenv.config();
+const db: Database = new Database();
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-let dbURI: string = '';
-let PORT: number;
-let HOST: string;
-
-if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.PRODUCTION_DB_URI!;
-    PORT = parseInt(process.env.PRODUCTION_PORT!);
-    HOST = process.env.PRODUCTION_HOST!;
-}
-
-if (process.env.NODE_ENV === 'development') {
-    dbURI = process.env.PRODUCTION_DB_URI!;
-    PORT = parseInt(process.env.PRODUCTION_PORT!);
-    HOST = process.env.PRODUCTION_HOST!;
-}
-
-connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
-    .then(() => {
-        app.listen(PORT, HOST, () => {
-            console.log(`App hosted on: ${HOST}:${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.log(`Mongodb Atlas Connection Error:  ${err.message}`);
-    });
-
-// Routes Middleware
+db.connection();
