1f15ec399f4da8470cb64c9c4b2c22c6b247144d
jillbhatt26 <bhattjill@gmail.com>
2021-07-29 22:39:39 +0530

    Removed useless comments

--- server/src/api/v1/routes/ShortUrlsRoutes.ts
+++ server/src/api/v1/routes/ShortUrlsRoutes.ts
@@ -14,34 +14,6 @@ import {
 // router init
 const router: Router = Router();
 
-// Swagger Docs
-
-/**
- * openapi: 3.0.0
- * info:
- *      title: Url Shortener API
- *      description: API to generate and manage short urls
- *      version: 0.1.0
- *
- *  servers:
- *      -url: http://localhost:5000/shortUrls
- *      description: Development server url
- *
- *  paths:
- *      /shortUrls:
- *          get:
- *              summary: Returns a list of all available shortUrls
- *              responses:
- *                  '200':
- *                      description: A JSON array of short urls
- *                      content:
- *                          application/json:
- *                              schema:
- *                                  type: array
- *                                  items:
- *                                      type: string
- */
-
 // routes definitions
 router.get('/home', Home);
 
